292f8636950f6fc98bed124268d90da0288102bc
CHIRAG3899 <66902259+CHIRAG3899@users.noreply.github.com>
2021-06-21 18:11:10 +0530

    Add files via upload

---
+++ Python Hackerrank/27 Designer Door Mat.py
@@ -0,0 +1,56 @@
+#Mr. Vincent works in a door mat manufacturing company. One day, he designed a
+#new door mat with the following specifications:
+#Mat size must be N X M. (N is an odd natural number, and M is 3 times N.)
+#The design should have 'WELCOME' written in the center.
+#The design pattern should only use |, . and - characters.
+#Sample Designs
+
+#    Size: 7 x 21 
+#    ---------.|.---------
+#    ------.|..|..|.------
+#    ---.|..|..|..|..|.---
+#    -------WELCOME-------
+#    ---.|..|..|..|..|.---
+#    ------.|..|..|.------
+#    ---------.|.---------
+    
+#    Size: 11 x 33
+#    ---------------.|.---------------
+#    ------------.|..|..|.------------
+#    ---------.|..|..|..|..|.---------
+#    ------.|..|..|..|..|..|..|.------
+#    ---.|..|..|..|..|..|..|..|..|.---
+#    -------------WELCOME-------------
+#    ---.|..|..|..|..|..|..|..|..|.---
+#    ------.|..|..|..|..|..|..|.------
+#    ---------.|..|..|..|..|.---------
+#    ------------.|..|..|.------------
+#    ---------------.|.---------------
+#Input Format
+#A single line containing the space separated values of N and M.
+#Output Format
+#Output the design pattern.
+#Sample Input
+#9 27
+#Sample Output
+
+#------------.|.------------
+#---------.|..|..|.---------
+#------.|..|..|..|..|.------
+#---.|..|..|..|..|..|..|.---
+#----------WELCOME----------
+#---.|..|..|..|..|..|..|.---
+#------.|..|..|..|..|.------
+#---------.|..|..|.---------
+#------------.|.------------
+
+n = int(input().split()[0])
+m = n*3
+line = []
+for i in range(1, n//2+1):
+    line.append(('.|.' * (i*2-1)).center(m, '-'))
+r = line[:]
+r.reverse()
+line.append('WELCOME'.center(m, '-'))
+print('\n'.join(line+r))
+
